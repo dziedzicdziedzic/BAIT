23c483c7586ca3426289e9a1725461371391afca
Dziedzic <Micha Dziedzic>
2017-11-25 15:29:40 +0100

    First commit

--- core/src/com/mygdx/game/ConveyorBelt.java
+++ core/src/com/mygdx/game/ConveyorBelt.java
@@ -1,23 +1,25 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 import java.util.Queue;
 
 public class ConveyorBelt {
-    private com.badlogic.gdx.utils.Queue<String> garbageQueue;
+    private com.badlogic.gdx.utils.Queue<BaseGarbage> garbageQueue;
+    Sprite belt;
     private int xspeed;
 
-    public void setGarbageQueue(com.badlogic.gdx.utils.Queue<String>  garbageQueue) {
+    public void setGarbageQueue(com.badlogic.gdx.utils.Queue<BaseGarbage>  garbageQueue) {
         this.garbageQueue = garbageQueue;
     }
 
-    public void render(){
-        xspeed = 1;
+    public void render(int xspeed){
+
         ShapeRenderer shapeRenderer = new ShapeRenderer();
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        for(int i=0; i<5/*garbageQueue.size*/; i++){
+        for(int i=0; i<5; i++){
             shapeRenderer.setColor(Color.BLACK);
             shapeRenderer.rect(50+60*i+xspeed, 50, 60, 100);
             shapeRenderer.setColor(Color.GRAY);
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,8 +2,10 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
@@ -14,37 +16,35 @@ import java.util.Queue;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	Texture conveyor_belt;
+	Sprite conveyor_belt;
 	ShapeRenderer shapeRenderer;
 	com.badlogic.gdx.utils.Queue queue;
-	boolean running = true;
+
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		conveyor_belt= new Texture("conveyor.png");
+		conveyor_belt = new Sprite(new Texture("C:\\Users\\Dziedzic\\Downloads\\test\\core\\assets\\conveyor.png"));
 		queue = new com.badlogic.gdx.utils.Queue();
 	}
 
 	@Override
 	public void render () {
+		int xspeed = 10;
 		ConveyorBelt cnb = new ConveyorBelt();
 		cnb.setGarbageQueue(queue);
-		while (running) {
 			Gdx.gl.glClearColor(255, 255, 255, 1);
 			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 			batch.begin();
-			cnb.render();
+			conveyor_belt.draw(batch);
+			conveyor_belt.translateX(10);
+
 			batch.end();
-		}
 	}
 
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
