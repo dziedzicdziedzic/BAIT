8a8f3c4e45f3f14fa1b5350ece64f15784d416da
DrewnoDrewno <ghostmichal@gmail.com>
2017-11-25 17:32:32 +0100

    Zdjęcia

--- core/src/com/mygdx/game/Person.java
+++ core/src/com/mygdx/game/Person.java
@@ -1,9 +1,6 @@
 package com.mygdx.game;
 //import java.lang.Object;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -11,53 +8,60 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Texture;
 
 public class Person {
-    private Sprite sprite;
+
     private SpriteBatch spriteBatch;
     private Texture texture;
-    //private Animation playerAnimation;
-    private TextureRegion[] playerTextureRegion;
-    private Texture playerAnimationTexture;
-    private TextureRegion currentFrame;
-    float stateTime = 0;
-    Animation<TextureRegion> playerAnimation;
+    private float timer;
+    private int counter;
+    private boolean ifhappy;
+    private Texture textures[]=new Texture[10];
 
 
-
-    public void load(){
-        //texture=new Texture("spacecraftanimation.png");
-        currentFrame = new TextureRegion();
-        playerAnimationTexture = new Texture("spacecraftanimation.png");
-
-        sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-        playerAnimationTexture = new Texture("spacecraftanimation.png");
-        playerTextureRegion = createRegion(playerTextureRegion, playerAnimationTexture, 100, 200, 3, 10);
-        playerAnimation = new Animation(0.1f, playerTextureRegion);
-        currentFrame = new TextureRegion();
-    }
-    public void render(){
-        stateTime += Gdx.graphics.getDeltaTime();
-        currentFrame = playerAnimation.getKeyFrame(stateTime, true);
-
-        spriteBatch.begin();
-        sprite.draw(spriteBatch);
-        spriteBatch.draw(currentFrame,30,30);
-        spriteBatch.end();
-    }
-    //x=100
-    //y=200
-    //row =10
-    //column=6
-    public static TextureRegion[] createRegion(TextureRegion[] outputRegion, Texture texture, int sizeX, int sizeY, int row, int column){
-
-        TextureRegion[][] tmp = TextureRegion.split(texture, sizeX, sizeY);
-        outputRegion = new TextureRegion[row*column];
-        int index = 0;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                outputRegion[index++] = tmp[i][j];
-            }
+    public void Happy(){
+        if(!ifhappy)return;
+        if(counter>10){
+            ifhappy=false;
+            counter=0;
         }
-        return outputRegion;
+        else {
+            ifhappy=true;
+        }
+        timer+=Gdx.graphics.getDeltaTime();
+        if(timer>=0.2){
+            timer=0;
+            counter++;
+        }
+        return;
     }
+
+    public void render(int ifhappy){
+
+      texture = new Texture("ludek.png");
+      Happy();
+      if(ifhappy==1){
+
+      }
+
+
+      if(ifhappy==1)return;
+      else timer=0;
+    }
+
+    public void initialization(){
+        textures[0]=new Texture("ludek.png");
+        textures[1]=new Texture("ludek2.png");
+        textures[2]=new Texture("ludek3.png");
+        textures[3]=new Texture("ludek4.png");
+        textures[4]=new Texture("ludek5.png");
+        textures[5]=new Texture("ludek6.png");
+        textures[6]=new Texture("ludek7.png");
+        textures[7]=new Texture("ludek8.png");
+        textures[8]=new Texture("ludek9.png");
+        textures[9]=new Texture("ludek10.png");
+        //textures[2]=new Texture("ludek3.png");
+        //we can do it as spritesheet :/
+
+    }
+
+
 }
