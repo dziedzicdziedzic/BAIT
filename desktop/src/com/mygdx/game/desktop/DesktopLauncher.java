350f2d8bd343fea7cbf15698d5f7aaa2b051f046
MrWhiteVolf <volf1997@gmail.com>
2017-11-25 17:16:08 +0100

    Smieci

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Fajny projekt hehe";
-		config.width = 800;
+		config.width = 801;
 		config.height = 600;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
