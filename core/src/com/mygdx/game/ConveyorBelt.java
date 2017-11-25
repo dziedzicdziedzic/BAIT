74b3111aa3e3c6f85243cb70039928c8c1517a01
MrWhiteVolf <volf1997@gmail.com>
2017-11-25 19:29:55 +0100

    Smieci

--- core/src/com/mygdx/game/ConveyorBelt.java
+++ core/src/com/mygdx/game/ConveyorBelt.java
@@ -1,18 +1,52 @@
 package com.mygdx.game;
-
-
-import java.util.Queue;
+import java.util.LinkedList;
 
 public class ConveyorBelt {
-    private Queue<BaseGarbage> garbageQueue;
+    private LinkedList<Garbage> garbageQueue;
 
-    public ConveyorBelt(Queue<BaseGarbage> garbageQueue) {
-        this.garbageQueue = garbageQueue;
+    Randomize randomize = new Randomize();
+
+    public ConveyorBelt() {
+        garbageQueue = new LinkedList<Garbage>();
     }
-    public void setGarbageQueue(Queue<BaseGarbage>  garbageQueue) {
-        this.garbageQueue = garbageQueue;
+
+    enum Type {
+        PLASTIC,GLASS,PAPER
     }
-    public BaseGarbage returnPopped(){
-        return garbageQueue.remove();
+    public void Setup()
+    {
+
+        int a = randomize.rand();
+        if (a==0)
+        {
+            garbageQueue.add(new Paper());
+        }
+        else
+        {
+            if(a==1) {
+                garbageQueue.add(new Glass());
+            }
+            else
+            {
+                garbageQueue.add(new Plastic());
+            }
+        }
+    }
+
+    public void SetupBelt()
+    {
+        for(int i=0; i<4; i++)
+        {
+            Setup();
+        }
+    }
+
+    public Type Shift()
+    {
+        Garbage element = garbageQueue.poll();
+        Setup();
+        if (element.returnType() == BaseGarbage.garbageType.PLASTIC) return Type.PLASTIC;
+        if (element.returnType() == BaseGarbage.garbageType.GLASS) return Type.GLASS;
+        else return Type.PAPER;
     }
 }
--- core/src/com/mygdx/game/Garbage.java
+++ core/src/com/mygdx/game/Garbage.java
@@ -8,11 +8,5 @@ public abstract class Garbage implements BaseGarbage {
 
     public abstract garbageType returnType();
     public abstract Texture getGarbageTex ();
-    public int Randomize()
-    {
-        Random generator = new Random();
-        int a = generator.nextInt(2);
-        return a;
 
-    }
 }
